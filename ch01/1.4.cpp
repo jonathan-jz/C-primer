dfea574f8f425b9b2cfbef2fbe696876fee28942
zhangshier <jonathonzhang16@gmail.com>
2024-03-04 21:29:45 +0800

    1.2节”

---
+++ ch01/1.4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+int main(){
+    int ch1,ch2;
+    std::cout << "input two nums:"<<std::endl;
+    std::cin >> ch1>>ch2;
+
+    std::cout <<"num1+num2 = "<<ch1+ch2<<std::endl;
+
+    std::cout <<"num1*num2 = "<<ch1*ch2<<std::endl;
+
+
+    return 0;
+}
