a612ce8ff471f33b50407229d7a0f7961631e77c
zhangshier <jonathonzhang16@gmail.com>
2024-03-04 22:14:35 +0800

    ch1  all ex

---
+++ ch01/1.23.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "Sales_item.h"
+
+int main() {
+    Sales_item currItem, valItem;
+    if (std::cin >> currItem) {
+        int cnt = 1;
+        while (std::cin >> valItem) {
+            if (valItem.isbn() == currItem.isbn()) {
+                ++cnt;
+            } else {
+                std::cout << currItem.isbn() << " occurs " << cnt << " times " << std::endl;
+                currItem = valItem;
+                cnt = 1;
+            }
+        }
+        std::cout << currItem.isbn() << " occurs " << cnt << " times " << std::endl;
+    }
+    return 0;
+}
+
